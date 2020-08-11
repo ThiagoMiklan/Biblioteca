526af52d2ef40806bdd8edd407fa9bec798d9fba
unknown <miklanthiago@gmail.com>
2020-08-11 18:21:51 -0300

    0.0.21 removed react-scripts

--- src/index.js
+++ src/index.js
@@ -2,11 +2,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bulma/css/bulma.css';
- 
+import Table from './components/Table';
 
+const elem = document.createElement('div')
+document.body.appendChild(elem)
+
+const itens_header = [
+  {value:"One"},{value:"Two"}
+]
+
+const itens_body =[
+  {One:"Three",Two:"Four"},
+  {Five:"Five", Six:"Six"},
+  {Nine:"Nine", Ten: "Ten"},
+  {Eleven:"Eleven", Twelve:"Twelve"}
+]
+
+const itens_footer = [
+    {value:"One Footer"},{value:"Two Footer"}
+]
 
 ReactDOM.render(
-     <h1>Hello World</h1>
-    ,
-    document.getElementById('root')
+    <Table definition="is-bordered" itens_header={itens_header} itens_body={itens_body} itens_footer={itens_footer}/>
+    ,elem
 );
