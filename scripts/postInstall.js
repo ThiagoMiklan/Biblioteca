dba8d5ffdd66766246eeabfe31592361d2a84ebb
unknown <miklanthiago@gmail.com>
2020-08-19 19:05:04 -0300

    0.0.24

---
+++ scripts/postInstall.js
@@ -0,0 +1,9 @@
+const colors = require('colors');
+
+const bulma_msg = "For the correct use of the Assemble React Bulma package, it is necessary that you use it in conjunction with the Bulma package (0.9.0)".yellow.bgRed.bold;
+
+const doc_msg = "For more information access: https://github.com/ThiagoMiklan/ARB_DOC/wiki".green;
+
+console.log(bulma_msg);
+
+console.log(doc_msg);
--- src/index.js
+++ src/index.js
@@ -1,37 +1,10 @@
-//@flow
-import React, { useState } from 'react';
-import ReactDOM from 'react-dom'
-import NavigationBar from './components/NavigationBar';
-import HeroThreeParts from './components/HeroThreeParts';
-import Title from './components/Title';
-import Subtitle from './components/Subtitle';
-import Tabs from './components/Tabs';
-import Container from './components/Container';
+// @flow
 import '../tests/css/bulma.css';
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
 
-const itens_navbar_start = [
-    {value:"Administration"},
-    {value:"General"},
-    {value:"About"}
-]
+ReactDOM.render(
+    <>
+    </>,document.getElementById("root"))
 
-const itens_navbar_end = [
-    {value:"Administration"},
-    {value:"General"},
-    {value:"About"}
-]
 
-const itens_tabs = [
-    {value:"Clients"},
-    {value:"Users"},
-    {value:"Requires"}
-]
-
-ReactDOM.render(<HeroThreeParts definition="is-fullheight">
-    <NavigationBar itens_start={itens_navbar_start} itens_end={itens_navbar_end} />
-    <Container definition="has-text-centered">
-        <Title definition={1}>Title</Title>
-        <Subtitle definition={2}>Subtitle</Subtitle>
-    </Container>
-    <Tabs itens={itens_tabs}></Tabs>
-</HeroThreeParts>, document.getElementById('root'));
